62cbd6df53ce0c166e7b7eaeeb2c69042e41c03d
wwiecek <20645798+wwiecek@users.noreply.github.com>
2020-05-11 12:29:28 +0100

    reindent UI script

--- shiny_ui.R
+++ shiny_ui.R
@@ -3,49 +3,54 @@ ui <- shinyUI(
     titlePanel("COVID-19 epidemic and intervention model"),
     tabsetPanel(
       id="main_tabset",
-      tabPanel("Basic simulation",
-               column(
-                 h3("Simulation settings"),
-                 selectInput("demographics", label = "Choose country (demographics)", 
-                             choices = c(countries), selected = "826"),
-                 plotOutput("plot_demographics", height = "200px"),
-                 uiOutput('panel1_settings'),
-                 checkboxInput('set_starting_toggle', "Set starting proportions?", value = FALSE),
-                 uiOutput('set_starting_ui'),
-                 dateInput('start_date', label = "Date of epidemic start", value = "2020-02-01"),
-                 width=2),
-               fluidRow(
-                 column(width=2,
-                        h3("NPI settings"),
-                        radioButtons("add_npi_toggle", "", 
-                                     choices = list("No intervention" = "no",
-                                                    "Basic NPI (same in all age groups)" = "basic",
-                                                    "Detailed NPI definition" = "detailed")),
-                        uiOutput("add_npi_ui")),
-                 column(width=2,
-                        h3("PI settings"),
-                        radioButtons("add_pi_toggle", "",
-                                     choices = list("No intervention" = "no",
-                                                    "Basic prophylaxis" = "basic_pro",
-                                                    "Basic active treatment" = "basic_at",
-                                                    "Vaccination" = "vac",
-                                                    "Detailed prophylaxis definition" = "detailed_pro")),
-                        uiOutput("add_pi_ui")),
-                 column(width = 5,
-                        h3("Simulation output"),
-                        flowLayout(
-                          selectInput("panel1_output_selector", 
-                                      "Display...", c(compartment_names_short), selected = "I1"),
-                          sliderInput("panel1_xlim", label = "Days to display", value=150, min=14, max=300),
-                          selectInput("panel1_scaling", "Y axis shows", c("Absolute numbers" = "absolute", 
-                                                                          "Numbers per 100,000" = "per100k",
-                                                                          "Percentages" = "pct"), selected = "absolute"),
-                          checkboxInput("panel1_dnmerge_groups", "Show individual age groups?", value = TRUE),
-                          checkboxInput("panel1_show_data", "Show real data?", value = FALSE)),
-                        plotOutput('panel1_plot')
-                        
-                 )
-               )
+      tabPanel(
+        "Basic simulation",
+        column(
+          h3("Simulation settings"),
+          selectInput("demographics", label = "Choose country (demographics)", 
+                      choices = c(countries), selected = "826"),
+          plotOutput("plot_demographics", height = "200px"),
+          uiOutput('panel1_settings'),
+          checkboxInput('set_starting_toggle', "Set starting proportions?", value = FALSE),
+          uiOutput('set_starting_ui'),
+          dateInput('start_date', label = "Date of epidemic start", value = "2020-02-01"),
+          width=2),
+        fluidRow(
+          column(
+            width=2,
+            h3("NPI settings"),
+            radioButtons("add_npi_toggle", "", 
+                         choices = list("No intervention" = "no",
+                                        "Basic NPI (same in all age groups)" = "basic",
+                                        "Detailed NPI definition" = "detailed")),
+            uiOutput("add_npi_ui")),
+          column(
+            width=2,
+            h3("PI settings"),
+            radioButtons("add_pi_toggle", "",
+                         choices = list("No intervention" = "no",
+                                        "Basic prophylaxis" = "basic_pro",
+                                        "Basic active treatment" = "basic_at",
+                                        "Vaccination" = "vac",
+                                        "Detailed prophylaxis definition" = "detailed_pro")),
+            uiOutput("add_pi_ui")),
+          column(
+            width = 5,
+            h3("Simulation output"),
+            flowLayout(
+              selectInput("panel1_output_selector", 
+                          "Display...", c(compartment_names_short), selected = "I1"),
+              sliderInput("panel1_xlim", label = "Days to display", value=150, min=14, max=300),
+              selectInput("panel1_scaling", "Y axis shows", 
+                          c("Absolute numbers" = "absolute", 
+                            "Numbers per 100,000" = "per100k",
+                            "Percentages" = "pct"), selected = "absolute"),
+              checkboxInput("panel1_dnmerge_groups", "Show individual age groups?", value = TRUE),
+              checkboxInput("panel1_show_data", "Show real data?", value = FALSE)),
+            plotOutput('panel1_plot')
+            
+          )
+        )
       ),
       tabPanel("Model description",
                includeHTML("doc/model_description.html")),
